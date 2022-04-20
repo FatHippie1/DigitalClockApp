d6c95944934212b199a59c329ffebc3f1d3f4f8d
FatHippie1 <80604504+FatHippie1@users.noreply.github.com>
2022-04-20 15:22:53 -0600

    Date line corrected

--- Scripts.js
+++ Scripts.js
@@ -1,2 +1,14 @@
-const d = new Date();
-document.getElementById("ClockHour").innerHTML = d;
+var d = new Date();
+var hour = d.getHours();
+var minute = d.getMinutes();
+var second = d.getSeconds();
+
+
+var date = d.getDate();
+var year = d.getFullYear();
+var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var month = ["January", "Feburary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+
+document.getElementById("clockHour").innerHTML = hour + ":" + minute + ":" + second + " " + "PM";
+document.getElementById("clockDate").innerHTML = day[d.getDay()] + " " + month[d.getMonth()] + " " + date + ", " + year;

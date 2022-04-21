9f2165c39f297380f9ceaacbf6e289e65be4bcf6
FatHippie1 <80604504+FatHippie1@users.noreply.github.com>
2022-04-21 15:24:39 -0600

    clock app working

--- Scripts.js
+++ Scripts.js
@@ -1,14 +1,36 @@
-var d = new Date();
-var hour = d.getHours();
-var minute = d.getMinutes();
-var second = d.getSeconds();
+function displayTime() {
+    var d = new Date();
+    var hour = d.getHours();
+    var minute = d.getMinutes();
+    var second = d.getSeconds();
+    var amPm = "AM";
 
+    if (hour > 12) {
+        hour = hour - 12;
+        amPm = "PM";
+    }
+    if (hour < 10) {
+        hour = "0" + hour;
+    } if (minute < 10) {
+        minute = "0" + minute;
+    }
+    if (second < 10) {
+        second = "0" + second;
+    }
 
-var date = d.getDate();
-var year = d.getFullYear();
-var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-var month = ["January", "Feburary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    document.getElementById("clockHour").innerHTML = hour + ":" + minute + ":" + second + " " + amPm;
+    let t = setInterval(function () { displayTime(), 1000 });
+}
 
+function displayDate() {
+    var d = new Date();
+    var date = d.getDate();
+    var year = d.getFullYear();
+    var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var month = ["January", "Feburary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-document.getElementById("clockHour").innerHTML = hour + ":" + minute + ":" + second + " " + "PM";
-document.getElementById("clockDate").innerHTML = day[d.getDay()] + " " + month[d.getMonth()] + " " + date + ", " + year;
+    document.getElementById("clockDate").innerHTML = day[d.getDay()] + " " + month[d.getMonth()] + " " + date + ", " + year;
+}
+
+displayTime();
+displayDate();
